ec3f3ffb7f7e796370f599636d81a7593210c28c
APF-MRoger <112392920+APF-MRoger@users.noreply.github.com>
2025-07-17 13:40:30 +0200

    TWAI daemon as separate component

--- right_screen/main/main.cpp
+++ right_screen/main/main.cpp
@@ -5,11 +5,9 @@
 #include <ui.h>
 #include "esp_timer.h"
 #include <math.h>
-#include "driver/twai.h"
+// #include "driver/twai.h"
+#include "twai_daemon.h"
 
-// CANBus IOs
-#define CAN_RX 43
-#define CAN_TX 44
 
 // Refresh interval to the LVGL objects
 #ifndef DISP_VALUES_REFRESH_INTERVAL
@@ -21,82 +19,7 @@ using namespace esp_panel::board;
 
 static const char *TAG = "GENERAL";
 
-#pragma region TWAI variables
-static const char *TWAI_TAG = "TWAI";
-// Only active if the TWAI_WATCHDOG is used
-#ifdef TWAI_WATCHDOG
-// Set up alerts filter
-uint32_t alerts_to_enable = TWAI_ALERT_RX_DATA |
-                            TWAI_ALERT_TX_FAILED |
-                            TWAI_ALERT_ERR_PASS |
-                            TWAI_ALERT_BUS_ERROR |
-                            TWAI_ALERT_RX_QUEUE_FULL |
-                            TWAI_ALERT_ARB_LOST;
-uint32_t twai_alerts_triggered;
-twai_status_info_t twai_status;
-unsigned long twai_wdg_rx_dropped = 0;
-unsigned long twai_wdg_rx_dropped_prev = 0;
-unsigned long twai_wdg_rx_dropped_rate = 0;
-#else
-uint32_t alerts_to_enable = TWAI_ALERT_NONE;
-#endif
 
-#define TWAI_POLL_MS 50
-static TaskHandle_t twai_task_handle = nullptr;
-void twai_task(void *arg) {
-    ESP_LOGI(TWAI_TAG, "TWAI task started");
-    twai_message_t rxMessage;
-    uint32_t rxCounter = 0;
-    while (true) {
-        while (twai_receive(&rxMessage, pdMS_TO_TICKS(0)) == ESP_OK) {
-            ESP_LOGD(TWAI_TAG, "Rxed: %lu", rxCounter++);
-        }
-        vTaskDelay(pdMS_TO_TICKS(TWAI_POLL_MS));
-    }
-    
-}
-
-
-void init_TWAI() {
-    twai_general_config_t g_config = {
-        .mode = TWAI_MODE_NORMAL,
-        .tx_io = (gpio_num_t)CAN_TX,
-        .rx_io = (gpio_num_t)CAN_RX,
-        .clkout_io = TWAI_IO_UNUSED,
-        .bus_off_io = TWAI_IO_UNUSED,
-        .tx_queue_len = 10,
-        .rx_queue_len = 256,
-        .alerts_enabled = alerts_to_enable,
-        .clkout_divider = 0,
-        .intr_flags = ESP_INTR_FLAG_LEVEL1,
-    };
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
-    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-
-    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
-        if (twai_start() != ESP_OK)
-        {
-            ESP_LOGE(TWAI_TAG,"Failed to start TWAI driver");
-        }
-        else
-        {
-            ESP_LOGI(TWAI_TAG,"TWAI driver started successfully");
-        }
-    }
-    else
-    {
-        ESP_LOGE(TWAI_TAG,"Failed to install TWAI driver");
-    }
-    BaseType_t twai_core_id = 0;
-    BaseType_t ret =xTaskCreatePinnedToCore(twai_task, "twai_daemon", 4096,NULL,5,&twai_task_handle,twai_core_id); 
-    if (ret != pdPASS) {
-        ESP_LOGE(TWAI_TAG, "Failed to create TWAI task");
-    } else {
-        ESP_LOGI(TWAI_TAG, "TWAI task created successfully");
-    }
-}
-
-#pragma endregion
 
 #pragma region Global variables
 
@@ -261,6 +184,13 @@ int updateLVGLObjects()
     return updatedElements;
 }
 
+esp_err_t dispatchFrame(twai_message_t *rxMsg)
+{
+    //Do nothing for now
+    return ESP_OK;
+}
+
+
 #pragma endregion
 
 #pragma region Main app
@@ -337,7 +267,11 @@ extern "C" void app_main()
     ESP_LOGI(TAG, "Backlight : %d", backLight->on());
 
     ESP_LOGI(TAG, "Starting TWAI port and daemon");
-    init_TWAI();
+    ESP_UTILS_CHECK_ERROR_EXIT(initCAN(&dispatchFrame), "Failed to initialize TWAI port and daemon");
+    // if (initCAN(&dispatchFrame) != ESP_OK)
+    // {
+    //     ESP_LOGE(TAG, "Failed to initialize TWAI port and daemon");
+    // }
     ESP_LOGI(TAG, "Setup done");
 
 #pragma region Main Loop
