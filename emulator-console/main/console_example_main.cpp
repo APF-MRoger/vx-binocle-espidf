9a7c77216afba3c91d4a79ed2c4fef74099b04fc
APF-MRoger <112392920+APF-MRoger@users.noreply.github.com>
2025-07-28 12:43:38 +0200

    Compartimentalized primary GPIO exp

--- emulator-console/main/console_example_main.cpp
+++ emulator-console/main/console_example_main.cpp
@@ -21,7 +21,8 @@
 #include "cmd_nvs.h"
 #include "gpio_defs.h"
 #include "pwm_console_cmds.h"
-#include <esp_io_expander.hpp>
+#include "gpio_exp_primary.h"
+
 
 /*
  * We warn if a secondary serial console is enabled. A secondary serial console is always output-only and
@@ -81,7 +82,6 @@ ledc_channel_t pwm_gen_coolant = LEDC_CHANNEL_0;
 ledc_channel_t pwm_gen_rpm = LEDC_CHANNEL_1;
 ledc_channel_t pwm_gen_speed = LEDC_CHANNEL_2;
 
-esp_expander::Base *activeHL_expander = nullptr;
 
 extern "C" void app_main(void)
 {
@@ -91,19 +91,7 @@ extern "C" void app_main(void)
     set_pwm_generator(LEDC_TIMER_2, SPEED_PWM_BASE_FREQ_HZ, (gpio_num_t)SPEED_PWM_GEN_GPIO, pwm_gen_speed, SPEED_PWM_BASE_DUTY_PCT);
 
     // Initialize the IO expander controlling the Active High and Low outputs
-    activeHL_expander = new esp_expander::HT8574(I2C_SCL_GPIO,I2C_SDA_GPIO,0x20);
-    if(activeHL_expander->init() == false)
-    {
-        ESP_LOGE(TAG,"Failed to initialize primary IO expander");
-    }
-    if(activeHL_expander->begin() == false)
-    {
-        ESP_LOGE(TAG,"Failed to begin the primary IO expander");
-    }
-    activeHL_expander->printStatus();
-    // activeHL_expander->pinMode(0,INPUT);
-    // printf("Pin 0: %u\n",activeHL_expander->digitalRead(0));
-
+    initialize_primary_expander();
 
     // Initialize console objects
     esp_console_repl_t *repl = NULL;
---
+++ emulator-console/main/gpio_exp_primary.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <esp_io_expander.hpp>
+#include "gpio_defs.h"
+
+#ifdef TAG
+#undef TAG
+#endif
+#define TAG "GPIO_EXP_PRIMARY"
+
+static esp_expander::Base *primary_expander = nullptr;
+
+esp_err_t initialize_primary_expander()
+{
+
+    primary_expander = new esp_expander::HT8574(I2C_SCL_GPIO,I2C_SDA_GPIO,0x20);
+    if(primary_expander->init() == false)
+    {
+        ESP_LOGE(TAG,"Failed to initialize primary IO expander");
+        return ESP_FAIL;
+    }
+    if(primary_expander->begin() == false)
+    {
+        ESP_LOGE(TAG,"Failed to begin the primary IO expander");
+        return ESP_FAIL;
+    }
+    primary_expander->printStatus();
+    return ESP_OK;
+}
