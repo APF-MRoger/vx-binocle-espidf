6982d172b41c54424d0a460487d2a68e0689fac0
APF-MRoger <112392920+APF-MRoger@users.noreply.github.com>
2025-08-04 16:45:53 +0200

    Cleanup

--- interface_board/main/main.cpp
+++ interface_board/main/main.cpp
@@ -31,12 +31,6 @@
 // PWM stats structures for coolant, rpm and speed captures
 static volatile pwm_info_t pwm_cap_coolant, pwm_cap_rpm, pwm_cap_speed = {.pos_edge_ts = 0, .prev_pos_edge_ts = 0, .period_ticks = 0, .neg_edge_ts = 0, .deltaT = 0};
 
-// Generator channels
-#if CONFIG_DEBUG_GENERATE_PWM
-ledc_channel_t pwm_gen_coolant = LEDC_CHANNEL_0;
-ledc_channel_t pwm_gen_rpm = LEDC_CHANNEL_1;
-ledc_channel_t pwm_gen_speed = LEDC_CHANNEL_2;
-#endif
 // Capture channels
 mcpwm_cap_channel_handle_t cap_chan_coolant = NULL;
 mcpwm_cap_channel_handle_t cap_chan_rpm = NULL;
@@ -54,7 +48,6 @@ void base_slow_metrics_PKG(void *pvParameters)
     // coolant_temp
     // fuel_level_pc
     // lv_voltage_v
-    
     while(true)
     {
         // SMAs should already be protected, and some of the MCPWM logic can be brought in here.      
@@ -186,11 +179,6 @@ extern "C" void app_main(void)
 
     // processor_task_hdl = xTaskGetCurrentTaskHandle();
     // To be removed later
-#if CONFIG_DEBUG_GENERATE_PWM
-    set_pwm_generator(LEDC_TIMER_0, CONFIG_COOLANT_PWM_BASE_FREQ_HZ, (gpio_num_t)CONFIG_COOLANT_PWM_GEN_GPIO, pwm_gen_coolant, CONFIG_COOLANT_PWM_BASE_DUTY_PCT);
-    set_pwm_generator(LEDC_TIMER_1, CONFIG_RPM_PWM_BASE_FREQ_HZ, (gpio_num_t)CONFIG_RPM_PWM_GEN_GPIO, pwm_gen_rpm, CONFIG_RPM_PWM_BASE_DUTY_PCT);
-    set_pwm_generator(LEDC_TIMER_2, CONFIG_SPEED_PWM_BASE_FREQ_HZ, (gpio_num_t)CONFIG_SPEED_PWM_GEN_GPIO, pwm_gen_speed, CONFIG_SPEED_PWM_BASE_DUTY_PCT);
-#endif
     // Set up the capture channels
     set_capture_channel(cap_chan_coolant, (gpio_num_t)CONFIG_COOLANT_PWM_CAP_GPIO, &pwm_cap_coolant);
     set_capture_channel(cap_chan_rpm, (gpio_num_t)CONFIG_RPM_PWM_CAP_GPIO, &pwm_cap_rpm);
@@ -235,11 +223,11 @@ extern "C" void app_main(void)
         // {
         //     ESP_LOGI(TAG,"SMA %u unscaled %.2f scaled %.2f V",i,smaOutputArray[i].unscaled,smaOutputArray[i].scaled);
         // }
-        for (int i = 0; i < 4; i++)
-        {
-            channels_raw[i] = sma_get_avg(adc_channels[i].sma);
-        }
-        ESP_LOGI(TAG,"Channels raw : %.2f %.2f %.2f %.2f",channels_raw[0],channels_raw[1],channels_raw[2],channels_raw[3]);
+        // for (int i = 0; i < 4; i++)
+        // {
+        //     channels_raw[i] = sma_get_avg(adc_channels[i].sma);
+        // }
+        // ESP_LOGI(TAG,"Channels raw : %.2f %.2f %.2f %.2f",channels_raw[0],channels_raw[1],channels_raw[2],channels_raw[3]);
         
 
 
@@ -265,15 +253,7 @@ extern "C" void app_main(void)
         pwm_cap_speed.deltaT = 0;
         pwm_cap_speed.period_ticks = 0;
 #endif
-#if CONFIG_DEBUG_GENERATE_PWM
-        start_duty_cycle = (start_duty_cycle + 1) % 99;
-        if (change_duty_cycle(pwm_gen_coolant, start_duty_cycle + 1) != ESP_OK)
-        {
-            ESP_LOGE(TAG, "Failed to change coolant duty cycle");
-        }
-        start_frequency = (start_frequency + 1) % 996;
-        change_frequency(pwm_gen_speed, 4 + start_frequency);
-#endif
+
 
         // portEXIT_CRITICAL(&counter_mux);
     }
